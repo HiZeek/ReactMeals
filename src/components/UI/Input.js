151a3d17bf58939d94e307367ae900a37056fcb5
Isaac Idibia <idibiaisaac3@gmail.com>
2022-08-05 23:03:51 +0100

    modal

--- src/components/UI/Input.js
+++ src/components/UI/Input.js
@@ -5,7 +5,7 @@ const Input = (props) => {
   return (
     <div className={classes.input}>
         <label htmlFor={props.input.id}>{props.label}</label>
-        <input id={props.input.id} type="number" />
+        <input {...props.input} />
     </div>
   );
 };
